05920d0da3e7ebe0a2043b1d5affb0b0c63012b2
Stephane.C <tchadizman@gmail.com>
2022-11-03 15:11:19 +0100

    SRP - Fill CommandPriceCalculator

--- CommandApi/Entity/CommandPriceCalculator.cs
+++ CommandApi/Entity/CommandPriceCalculator.cs
@@ -1,6 +1,20 @@
-﻿namespace CommandApi.Entity
+﻿using CommandApi.Controllers;
+
+namespace CommandApi.Entity
 {
-    public class PriceCalculator
+    public class CommandPriceCalculator
     {
+        internal double GetPriceTtc(Command command)
+        {
+            double commandPrice = 0;
+            foreach (var produitquandtite in command.ProduitCommande)
+            {
+                double prixProduit = TarifHelper.GetPrixByProduit(produitquandtite.Item1.Id);
+                prixProduit = prixProduit * produitquandtite.Item2;
+                commandPrice += prixProduit;
+            }
+            return commandPrice;
+
+        }
     }
 }
